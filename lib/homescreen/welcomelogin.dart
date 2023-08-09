61cc9fd5342e7e3e80d913d76945750b55a60511
Unique-chiemerie <lilanimate1@gmail.com>
2023-08-09 14:38:23 +0100

    login and  sign up screen

--- lib/homescreen/welcomelogin.dart
+++ lib/homescreen/welcomelogin.dart
@@ -5,6 +5,10 @@ class welcomeS extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
