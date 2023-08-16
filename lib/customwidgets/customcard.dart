4d0e3a995f9f35f0dbb8c8076d04312160c3b104
user is blnk <132100200+Unique-chiemerie@users.noreply.github.com>
2023-08-16 13:54:01 +0100

    building the activity card bro

---
+++ lib/customwidgets/customcard.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CarD extends StatelessWidget {
+  const CarD({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 230,
+      width: 300,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 7,
+            color: Color.fromARGB(255, 236, 235, 235),
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
--- lib/dashboard/activityboard.dart
+++ lib/dashboard/activityboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workout_ui/customwidgets/customcard.dart';
 import 'package:workout_ui/customwidgets/glowbutton.dart';
 
 class activityb extends StatelessWidget {
@@ -40,16 +41,26 @@ class activityb extends StatelessWidget {
               ),
             ],
           ),
-
+          const SizedBox(
+            height: 30,
+          ),
           //this is where the user info is dislpayed
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 70,
-                width: 70,
-                color: Colors.green,
+              const SizedBox(
+                width: 30,
+              ),
+              SizedBox(
+                height: 50,
+                width: 50,
+                child: Image.asset('ui/sippin.png'),
+              ),
+              const SizedBox(
+                width: 10,
               ),
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     username,
@@ -62,9 +73,185 @@ class activityb extends StatelessWidget {
                   ),
                 ],
               ),
-              Glow(buttontext: 'Edit')
+              const SizedBox(
+                width: 50,
+              ),
+              SizedBox(
+                height: 50,
+                width: 100,
+                child: Glow(buttontext: 'Edit'),
+              ),
             ],
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          //this is the metric info "age", "weight", " height"
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 70,
+                width: 100,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 7,
+                      color: Color.fromARGB(255, 236, 235, 235),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: //the metadata/metrics that goes into the container
+                    const Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '180cm',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 157, 154, 246),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Height',
+                      style: TextStyle(
+                        fontFamily: 'raleway',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 100,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 7,
+                      color: Color.fromARGB(255, 236, 235, 235),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                //the weight metric goes here
+                child: const Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '65kg',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 157, 154, 246),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'weight',
+                      style: TextStyle(
+                        fontFamily: 'raleway',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 100,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 7,
+                      color: Color.fromARGB(255, 236, 235, 235),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                //the age metric goes here
+                child: const Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '19yo',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 157, 154, 246),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Age',
+                      style: TextStyle(
+                        fontFamily: 'raleway',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          ///done with the metrics row now it's time
+          ///for the activity which has got alot of icons , so to keep the ui clean we could build
+          ///a new widget instead and just dey import am inside the screens to keep the codes clean ofc
+          CarD(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Activity',
+                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 300,
+                color: Colors.red,
+                margin: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('ui/pp.png'),
+                    ),
+                    const Text(
+                      'Personal Data',
+                      style: TextStyle(
+                          fontFamily: 'raleway', fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ))
         ],
       ),
     );
